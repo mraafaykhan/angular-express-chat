98a311c272d11aa0178d79815982852a33102718
Raafay khan <rafay.khan@strendent.com>
2018-03-19 12:28:06 +0500

    added home, register, login component to angular app

--- clientapp/src/app/app.module.ts
+++ clientapp/src/app/app.module.ts
@@ -4,18 +4,30 @@ import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
